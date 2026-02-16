eb8d15b81ba71e4ce988e56290251c8a773319a9
Suboc <argie.suboc@iqor.com>
2026-02-16 16:20:42 -0500

    day-11 done

---
+++ day-11/script3.js
@@ -0,0 +1,30 @@
+
+
+let score =[80, 75, 90, 60, 85]
+let total = 0
+for(let i = 0; i<score.length;i++){
+  total += score[i]
+
+
+}
+let average = total / score.length
+
+console.log(total)
+console.log(average)
+let numbers =[2, 8, 3, 10, 4]
+let num = []
+
+for(let i= 0;i<numbers.length;i++){
+  if(numbers[i]>5){
+    num.push(numbers[i])
+  }
+}
+console.log(num)
+let numbs = [3, 4, 5]
+let multiply = []
+for( let i=0;i<numbers.length;i++){
+   multiply += numbers[i]*2
+
+}
+console.log(multiply)
+
